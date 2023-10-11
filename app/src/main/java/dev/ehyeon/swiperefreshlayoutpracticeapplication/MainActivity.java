def8ca972ff81c324b3e1254ba398fa7ba838f32
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-11 18:17:01 +0900

    feat: SwipeRefreshLayout 적용

--- app/src/main/java/dev/ehyeon/swiperefreshlayoutpracticeapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/swiperefreshlayoutpracticeapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package dev.ehyeon.swiperefreshlayoutpracticeapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        TextView textView = findViewById(R.id.textView);
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            textView.setText("Hello World! " + ++count);
+
+            swipeRefreshLayout.setRefreshing(false);
+        });
     }
 }
